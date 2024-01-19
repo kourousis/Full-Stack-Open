7e4dbddbce63075e81e8b2fd4e848d8086a18739
Ilias Kourousis <i.k.kourousis@gmail.com>
2024-01-19 02:25:45 +0000

    completed exercies 2.7

--- Part 2/2.6 Phonebook/src/App.jsx
+++ Part 2/2.6 Phonebook/src/App.jsx
@@ -13,8 +13,12 @@ const App = () => {
       name: newName
     }
 
-    setPersons(persons.concat(nameObject))
-    setNewName('')
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+    }
   }
 
   const handleNameChange = (event) => {
