4755cb020659933b8fb5b4fb3a1bd60261d71a17
Ilias Kourousis <i.k.kourousis@gmail.com>
2024-01-22 02:06:41 +0000

    person array expansion & formatting

--- Part 2/2.6 Phonebook/src/App.jsx
+++ Part 2/2.6 Phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import Name from './components/Names'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 const App = () => {
   const [newName, setNewName] = useState('')
@@ -7,10 +7,25 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: 'Arto Hellas',
-      number: '040-1234567'
+      number: '040-123456',
+      id: 1
+    },
+    {
+      name: 'Ada Lovelace',
+      number: '39-44-5323523',
+      id: 2
+    },
+    {
+      name: 'Dan Abramov',
+      number: '12-43-234345',
+      id: 3
+    },
+    {
+      name: 'Mary Poppendieck',
+      number: '39-23-6423122',
+      id: 4
     }
   ])
-
 
   const addName = (event) => {
     event.preventDefault()
@@ -42,6 +57,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input />
+        </div>
+      </form>
+      <div>
+        <h2>add a new</h2>
+      </div>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} /> <br />
